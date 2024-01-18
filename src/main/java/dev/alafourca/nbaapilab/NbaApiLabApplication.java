57dba9fcf8596ccb94209759ace92e08ae38e3f2
Ale <alafourcadedespaigne@gmail.com>
2024-01-17 22:32:31 -0300

    Project Structure

---
+++ src/main/java/dev/alafourca/nbaapilab/NbaApiLabApplication.java
@@ -0,0 +1,13 @@
+package dev.alafourca.nbaapilab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbaApiLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbaApiLabApplication.class, args);
+	}
+
+}
